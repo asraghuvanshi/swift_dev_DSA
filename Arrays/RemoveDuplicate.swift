998610feaa2a00bdc39827c7183e9cdb72f779b3
Amit <asraghuvanshi52@gmail.com>
2026-01-03 22:21:14 +0530

    Remove duplicate

---
+++ Arrays/RemoveDuplicate.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+class Solution {
+    func removeDuplicate(arr: [Int]) {
+        for i in arr {
+           if !uniqueElement.insert(i).inserted {
+               uniqueElement.insert(i)
+           }
+        }
+        print(uniqueElement)
+    }
+
+    func removeDuplicate1(arr: [Int]) {
+        var uniqueElement = Array<Int>()
+        for i in 0..<arr.count {
+            var isDuplicate: Bool = false
+            for j in 0..<uniqueElement.count {
+                if arr[i] == uniqueElement[j] {
+                    isDuplicate = true
+                    break
+                }
+            }
+            if !isDuplicate {
+                uniqueElement.append(arr[i])
+            }
+        }
+        print(uniqueElement)
+    }
+}
+
+let sol = Solution().removeDuplicate1(arr: [1,4,5,6,1,5])
+print(sol)
+
+
