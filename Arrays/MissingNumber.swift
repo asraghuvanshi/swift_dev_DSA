6e287059761bc787ae4a9c7825b211e380423b4b
Amit <asraghuvanshi52@gmail.com>
2026-03-15 00:34:11 +0530

    Missing number array

---
+++ Arrays/MissingNumber.swift
@@ -0,0 +1,29 @@
+/* 
+   Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+   Example 1:
+   Input: nums = [3,0,1]
+
+   Output: 2
+   Explanation:
+      n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+*/
+class Solution {
+    // func missingNumber(_ nums: [Int]) -> Int {
+    //    let n = nums.count 
+    //    let sum = (n * (n + 1)) / 2
+    //    print(sum)
+
+    //    let totalArrSum = nums.reduce(0 , +)
+    //    return sum - totalArrSum
+    // }
+
+     func missingNumber(_ nums: [Int]) -> Int {
+        let set = Set(nums)
+        for i in 0...nums.count {
+            if !set.contains(i) {
+                return i
+            }
+        }
+        return 0
+    }
+}
